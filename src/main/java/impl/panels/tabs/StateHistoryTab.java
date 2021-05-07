c19adf0b4201b3e433854676f49a88453b8381ae
matic277 <matic277@gmail.com>
2021-05-07 18:01:24 +0200

    Code cleanup

--- src/main/java/impl/panels/tabs/StateHistoryTab.java
+++ src/main/java/impl/panels/tabs/StateHistoryTab.java
@@ -26,22 +26,15 @@ public class StateHistoryTab extends JPanel implements Observer {
         this.setLayout(new FlowLayout());
         this.setMinimumSize(new Dimension(0, 0));
         
-        int padding = 5;
         int width  = 45;
-        int height = 30; // ?? flowlayout
+        int height = 30;
         
-        //for (int i = 0, x = padding; i<NUM_OF_STATES; i++, x+=width+padding) { // when bounds is a rectangle and we draw it manually
-        for (int i = 0; i<NUM_OF_STATES; i++) { // when the bound is a jlabel
+        for (int i = 0; i<NUM_OF_STATES; i++) {
             int finalI = i;
             var box = new JButton() { public int index = finalI; };
             box.setText(i+"");
             box.setFont(Tools.getFont(14));
-//            box.setBounds( // rectangle bound
-//                    x + bounds.x,
-//                    padding + bounds.y,
-//                    width,
-//                    height);
-            box.setPreferredSize(new Dimension(width, height)); // bounded by jlabel with flowlayout
+            box.setPreferredSize(new Dimension(width, height));
             box.addActionListener(a -> {
                 AlgorithmController.currentStateIndex = box.index;
                 box.setBorderPainted(true);
@@ -61,34 +54,16 @@ public class StateHistoryTab extends JPanel implements Observer {
         }
         // outline first button
         stateBox[0].setBorder(SELECTED_BORDER);
-
     }
-    
     
     @Override
     public void notifyStateChange(int newStateIndex) {
         for (int i=0; i<stateBox.length; i++) {
             stateBox[i].setBorder(DEFAULT_BORDER);
-            if (i <= newStateIndex) stateBox[i].setEnabled(true);
-            else stateBox[i].setEnabled(false);
+            stateBox[i].setEnabled(i <= newStateIndex); // enable new buttons
         }
         if (newStateIndex <= stateBox.length-1) {
             stateBox[newStateIndex].setBorder(SELECTED_BORDER);
         }
     }
-    
-    public static class RoundBorder implements Border {
-        Color clr;
-        private final int rad;
-        public RoundBorder(Color clr, int rad) { this.rad = rad; this.clr = clr; }
-        
-        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-            g.setColor(clr);
-//            ((Graphics2D) g).setStroke(Tools.BOLD_STROKE);
-            g.drawRoundRect(x+1, y+1, width-3, height-3, rad, rad);
-        }
-        public boolean isBorderOpaque() { return true; }
-        public Insets getBorderInsets(Component c) { return new Insets(this.rad+1, this.rad+1, this.rad+2, this.rad); }
-    }
-    
 }
