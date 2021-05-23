a333bbca9d10551a007e71d8856c29d55dc7633e
matic277 <matic277@gmail.com>
2021-05-23 23:33:20 +0200

    Responsive info on graph re-layout, unfinished

--- src/main/java/impl/panels/tabs/ReLayoutTab.java
+++ src/main/java/impl/panels/tabs/ReLayoutTab.java
@@ -42,39 +42,72 @@ public class ReLayoutTab extends JPanel {
         layoutTypeDropdown.setEnabled(true);
         layoutTypeDropdown.addActionListener(a -> selectedLayout = (LayoutType)layoutTypeDropdown.getSelectedItem());
         
-        JLabel errorLabel = new JLabel();
+        JLabel errorlbl = new JLabel();
         // creating borders makes label visible even when
         // visibility is set to FALSE; -> swing bug??
 //        errorLabel.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Tools.RED));
-        errorLabel.setFont(Tools.getMonospacedFont(14));
-        errorLabel.setForeground(Tools.RED);
-        errorLabel.setVisible(true); // off by default
+        errorlbl.setFont(Tools.getMonospacedFont(14));
+        errorlbl.setForeground(Tools.RED);
+        errorlbl.setVisible(true); // off by default
+    
+        JLabel processingLbl = new JLabel();
+        processingLbl.setFont(Tools.getMonospacedFont(14));
+        processingLbl.setForeground(Tools.GREEN);
+        processingLbl.setVisible(true); // off by default
         
         doLayoutBtn = new JButton("Apply");
         doLayoutBtn.setFont(Tools.getFont(14));
         doLayoutBtn.addActionListener(a -> {
             try {
-                GraphBuilder.layoutTypeMap.get(selectedLayout).run();
+                doLayoutBtn.setEnabled(false);
+                
+                CompletableFuture.runAsync(() -> {
+                    // run layout algorithm
+                    // signal with processing label
+                    System.out.println("here");
+                    processingLbl.setText(" Doing layout... ");
+                    processingLbl.setBorder(new Tools.RoundBorder(Tools.GREEN, new BasicStroke(2), 10));
+                    processingLbl.setPreferredSize(new Dimension((int) processingLbl.getPreferredSize().getWidth(), 30));
+                    processingLbl.setVisible(true);
+                    
+                    GraphBuilder.layoutTypeMap.get(selectedLayout).run();
+                    
+                    processingLbl.setText(" Done! ");
+                    Tools.sleep(1000);
+                    processingLbl.setVisible(false);
+                    System.out.println("done");
+                    
+                }).exceptionally(e -> {
+                    System.out.println("error");
+                    doLayoutBtn.setEnabled(true);
+                    // on error signal message
+                    processingLbl.setVisible(false);
+                    
+                    errorlbl.setText(" " + e.getLocalizedMessage() + " ");
+                    errorlbl.setBorder(new Tools.RoundBorder(Tools.RED, new BasicStroke(2), 10));
+                    errorlbl.setPreferredSize(new Dimension((int) errorlbl.getPreferredSize().getWidth(), 30));
+                    errorlbl.setVisible(true);
+                    
+                    Tools.sleep(5000);
+                    errorlbl.setVisible(false);
+                    return null;
+                }).thenApply((x) -> {
+                    doLayoutBtn.setEnabled(true);
+                    return null;
+                });
+               
             } catch (Exception e) {
                 // catch JGraphT exceptions, some graphs can't be laid out
                 // in selected way. Like bipartite layout.
-                errorLabel.setText(" " + e.getLocalizedMessage() + " ");
-//                errorLabel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 10));
-                errorLabel.setBorder(new Tools.RoundBorder(Tools.RED, new BasicStroke(2), 10));
-                errorLabel.setPreferredSize(new Dimension((int) errorLabel.getPreferredSize().getWidth(), 30));
-                errorLabel.setVisible(true);
-                // Turn error reporting off after some seconds
-                CompletableFuture.runAsync(() -> {
-                    Tools.sleep(5000);
-                    errorLabel.setVisible(false);
-                });
+                
             }
         });
         
         this.add(info);
         this.add(layoutTypeDropdown);
         this.add(doLayoutBtn);
-        this.add(errorLabel);
+        this.add(errorlbl);
+        this.add(processingLbl);
         
         this.repaint();
     }
